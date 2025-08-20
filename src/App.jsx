4e29a42e595b677b6df8bf89ea9c8777a3472925
ali-nass <ali.slman7.98@gmail.com>
2025-08-20 19:25:56 +0300

    coming soon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/images/plane.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+   
   return (
-    <>
-      <h1 className='text-green-700 text-2xl font-medium'>Hello World!!</h1>
-    </>
+   <div className="flex items-center justify-center w-screen h-screen bg-linen">
+      <div className="text-center">
+          <h1 className="text-3xl font-bold font-lato md:text-5xl text-mgreen ">
+            Coming Soon ..
+          </h1>
+        <div className="flex justify-center mt-6">
+          <img src={reactLogo}  className="w-73 text-mgreen animate-pulse" alt="sss" />
+        </div>
+ 
+      </div>
+    </div>
   )
 }
 
